da1e71875b9e8b1eeb43fdc131606cdfbea13ee8
Bionca Bond <bioncab@zillowgroup.com>
2022-02-03 22:02:56 -0800

    not sure what is wrong with my Queue call? Randomly broke when doing the last test

---
+++ python/trees/breadth_first.py
@@ -0,0 +1,36 @@
+try:
+    from trees.node import Node
+    from trees.binary_tree import Binary_Tree
+    from stack_and_queues.stack_and_queue.queue import Queue
+
+except:
+    from trees.node import Node
+    from trees.binary_tree import Binary_Tree
+
+
+
+
+def breadth_first(bt):
+    queue = Queue()
+    queue.enqueue(bt.root)
+    output_list = []
+
+    while queue.IsEmpty() == None:
+        return "There is no tree to see."
+
+    else:
+        if bt.root:
+            output_list.append(bt.root)
+
+        while queue:
+            current = queue.dequeue()
+            output_list.append(current.value)
+            if current.left:
+                queue.enqueue(current.left)
+            if current.right:
+                queue.enqueue(current.right)
+        return output_list
+
+
+
+
